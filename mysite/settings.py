12d94dbbbca3a102e8c6df5d017e3d932d1343b6
ds10x2 <padasol@khu.ac.kr>
2023-05-19 16:34:37 +0900

    add allowed host

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,8 @@ SECRET_KEY = 'django-insecure-$*=ce*7jy*-pdd3nc55n!^gm9jeu%2u5ofe&fkphbr^e3mfnme
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['ds10x2.pythonanywhere.com', 'yourdomain.com']
+
 
 
 # Application definition
