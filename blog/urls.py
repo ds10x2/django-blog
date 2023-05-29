cd231c0d3fe01c738927352edcc4c80693fdf542
ds10x2 <padasol@khu.ac.kr>
2023-05-29 17:09:19 +0900

    Added view and template for detailed blog post as well as CSS for the site

--- blog/urls.py
+++ blog/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.post_list, name='post_list'),
-    path('poem/', views.poem, name='poem')
+    path('poem/', views.poem, name='poem'),
+    path('post/<int:pk>/', views.post_detail, name='post_detail'),
 ]
