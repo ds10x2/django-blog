71a1687bf7ed85914bba987e3f639fe2f20b8da7
ds10x2 <padasol@khu.ac.kr>
2023-06-10 22:10:11 +0900

    REST API

--- blog/models.py
+++ blog/models.py
@@ -8,7 +8,7 @@ class Post(models.Model):
     title = models.CharField(max_length=200)
     text = models.TextField()
     created_date = models.DateTimeField(default=timezone.now)
-    published_date = models.DateField(blank=True, null=True)
+    published_date = models.DateTimeField(blank=True, null=True)
     image = models.ImageField(upload_to='intruder_image/%Y/%m/%d/', default='intruder_image/default_error.png')
 
     def publish(self):
